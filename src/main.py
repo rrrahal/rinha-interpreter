6315bcad526bb14bc9de4e826981c4739b2d15a5
Rafael Rahal <rrrahal@gmail.com>
2023-10-06 10:48:29 -0300

    fix code organization

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from interpreter import interpret
 
 def main():
   # read the file from souce/print.json
-  source_code = open('source/combination.json', 'r').read()
+  source_code = open('examples/fib.json', 'r').read()
   AST = json.loads(source_code)
   interpret(AST)
 
